6c86fe446a6f637d518112d0d39aa4b19ba6e067
Sonal Singh <sonal1201kumar@gmail.com>
2025-10-29 18:14:31 +0530

    Done with setup and navigation bar

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "BrainDeck",
+  description: "Save hours of reading Docs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable}  font-sans antialiased`}>
+        <div className="min-h-screen flex flex-col mx-auto relative max-w-7xl">
+        <Header/>
+        <main className="flex-1">{children}</main>
+        <Footer/>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <div>
+    <h1 className="flex justify-center items-center font-bold text-5xl">BrainDeck</h1>
+  </div>;
+}
